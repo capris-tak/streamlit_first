dc3f454bc90ba593bb747a171bc8b1fc7d5d8c08
capris-tak <109158237+capris-tak@users.noreply.github.com>
2022-07-14 13:01:30 +0900

    Add files via upload

---
+++ run.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#cd Documents/JupytarNotebook
+#jupyter nbconvert --to python run.ipynb
+#streamlit run run.py
+
+
+# In[1]:
+
+
+import streamlit as st
+st.title('streamlitでWebアプリを最速で作ってネット公開')
+st.caption('サプーの')
+st.subheader('VTuber サプー')
+         
+code = """
+import streamlit as st
+st.title('streamlitでWebアプリを最速で作ってネット公開)
+st.caption('サプーの')
+st.subheader('VTuber サプー')
+#画像
+image = Image.open('wordcloud.png')
+st.image(image, width=500)
+#動画
+video_file = video('XXXXX.mov', 'rb')
+video_bytes = video_file.read()
+st.video(video_bytes)
+"""
+st.code(code, language='python')
+
+#テキストボックス
+name = st.text_input('名前')
+print(name)
+
+#ボタン
+
+#複数選択
+hobby = st.multiselect(
+    '趣味',
+    ('スポーツ','読書','プログラミング','映画鑑賞','釣り','料理'))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
