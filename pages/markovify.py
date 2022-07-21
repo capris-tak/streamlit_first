994487af8891ddb0bf4cc3313cfb40bc0dac4b20
capris-tak <109158237+capris-tak@users.noreply.github.com>
2022-07-21 16:28:25 +0900

    Update markovify.py

--- pages/markovify.py
+++ pages/markovify.py
@@ -4,7 +4,7 @@ st.write('マルコフ連鎖で文章自動生成')
 st.write('https://qiita.com/mapps/items/c0d3f1b73bc9ef398790')
 
 #import Tokenizer
-import markovify
+#import markovify
 file = 'pages/Hermann_Hesse.txt' 
 f = open(file, 'r', encoding="utf-8")
 text = f.read()
