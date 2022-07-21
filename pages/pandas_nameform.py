6f35a49b57e28a0a762b8a43b8d8a079f3f999fb
capris-tak <109158237+capris-tak@users.noreply.github.com>
2022-07-21 14:36:46 +0900

    Update pandas_nameform.py

--- pages/pandas_nameform.py
+++ pages/pandas_nameform.py
@@ -13,3 +13,18 @@ st.dataframe(data.head(5))
 st.write('苗字surnameの個数:',str(data["苗字"].count()))
 st.write('男性maleの名前の個数:',str(data["名前(男)"].count()))
 st.write('女性femaleの名前の個数:',str(data["名前(女)"].count()))
+
+#男性  乱数シードを設定
+np.random.seed(10)
+# 乱数を生成
+rd_s = np.random.randint(0, ct_s)
+rd_m = np.random.randint(0, ct_m)
+# 氏名を作成
+name = data.loc[rd_s, "苗字"]\
+       + " " + data.loc[rd_m, "名前(男)"]
+# ふりがな(phonetic)を作成
+ph = data.loc[rd_s, "ふりがな1"]\
+     + " " + data.loc[rd_m, "ふりがな2"]
+# 氏名とふりがなをタプルにまとめる
+name = (name, ph)
+st.write(name)
