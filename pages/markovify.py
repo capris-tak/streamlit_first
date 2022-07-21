fa1e34eaa100d7b24ed3dc3c46c33c886d9f1522
capris-tak <109158237+capris-tak@users.noreply.github.com>
2022-07-21 16:26:37 +0900

    Update markovify.py

--- pages/markovify.py
+++ pages/markovify.py
@@ -6,7 +6,7 @@ https://www.youtube.com/watch?v=I_2WlQDPQOA
 https://qiita.com/mapps/items/c0d3f1b73bc9ef398790
 ''')
 
-from janome.tokenizer import Tokenizer
+import Tokenizer
 import markovify
 file = 'pages/Hermann_Hesse.txt' 
 f = open(file, 'r', encoding="utf-8")
