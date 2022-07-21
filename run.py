0919f49abd42d52e3c71e7df0918a73ed5e94f9b
capris-tak <109158237+capris-tak@users.noreply.github.com>
2022-07-21 10:08:01 +0900

    Update run.py

--- run.py
+++ run.py
@@ -1,15 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
-#cd Documents/JupytarNotebook
-#jupyter nbconvert --to python run.ipynb
-#streamlit run run.py
-
-
-# In[1]:
 import streamlit as st
 
 import pandas as pd
