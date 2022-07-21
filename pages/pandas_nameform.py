04e607edf9356b81f91cd665f67e2eeb4b2a195d
capris-tak <109158237+capris-tak@users.noreply.github.com>
2022-07-21 14:29:49 +0900

    Update pandas_nameform.py

--- pages/pandas_nameform.py
+++ pages/pandas_nameform.py
@@ -7,5 +7,5 @@ u = "https://python.atelierkobato.com/wp-content/uploads/2019/08/name_s.csv"
 # ファイルをデータフレームとして読み込む
 data = pd.read_csv(u,  encoding="SHIFT-JIS")
 
-st.write(len(data))
+st.write(str(len(data)))
 st.dataframe(data.head(5))
