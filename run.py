fbdd1058a44b06e9b4fec22a5fac73e99b7605c8
capris-tak <109158237+capris-tak@users.noreply.github.com>
2022-07-21 10:06:26 +0900

    Update run.py

--- run.py
+++ run.py
@@ -19,7 +19,5 @@ st.title('Home')
 st.write('1 基本操作　　2 Streamlit　3 数学　4 文字文章操作　5 画像')
 
 
-link = '[話題のPyScript！](http://capris.html.xdomain.jp/python/index.html)'
+link = '[話題のPyScript！　HTMLファイルにPythonが書けるWebブラウザで動くPython](http://capris.html.xdomain.jp/python/index.html)'
 st.markdown(link, unsafe_allow_html=True)
-st.write('HTMLファイルにPythonが書けるWebブラウザで動くPython')
-
