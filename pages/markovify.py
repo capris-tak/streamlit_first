34880871dc31fedbcf915da299495d24b3647919
capris-tak <109158237+capris-tak@users.noreply.github.com>
2022-07-21 16:38:19 +0900

    Update markovify.py

--- pages/markovify.py
+++ pages/markovify.py
@@ -8,3 +8,7 @@ st.write('https://qiita.com/mapps/items/c0d3f1b73bc9ef398790')
 file = 'pages/Hermann_Hesse.txt' 
 f = open(file, 'r', encoding="utf-8")
 text = f.read()
+
+st.write(text)
+
+#st.write(pages/Hermann_Hesse.txt)
