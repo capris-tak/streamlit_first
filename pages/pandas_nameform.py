571ad884123dc691687b9d84aba98a9a88c72640
capris-tak <109158237+capris-tak@users.noreply.github.com>
2022-07-21 14:29:14 +0900

    Update pandas_nameform.py

--- pages/pandas_nameform.py
+++ pages/pandas_nameform.py
@@ -1,7 +1,11 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
 # ファイルのURL
 u = "https://python.atelierkobato.com/wp-content/uploads/2019/08/name_s.csv"
 # ファイルをデータフレームとして読み込む
 data = pd.read_csv(u,  encoding="SHIFT-JIS")
+
+st.write(len(data))
+st.dataframe(data.head(5))
