1ebfe86e53bd0eb90eff90ba702003c946376e8f
capris-tak <109158237+capris-tak@users.noreply.github.com>
2022-12-15 13:18:24 +0900

    Update run.py

--- run.py
+++ run.py
@@ -6,6 +6,11 @@ import numpy as np
 st.title('Home')
 st.write('1 基本操作　　2 Streamlit　3 数学　4 文字文章操作　5 画像')
 
+code = '''def hello():
+    print("Hello, Streamlit!")'''
+st.code(code, language='python')
+
+
 
 link = '[話題のPyScript！　HTMLファイルにPythonが書けるWebブラウザで動くPython](http://capris.html.xdomain.jp/python/index.html)'
 st.markdown(link, unsafe_allow_html=True)
