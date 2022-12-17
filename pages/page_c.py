0ae21366a16c8a6c8079d79ff78c10a9722fa696
capris-tak <109158237+capris-tak@users.noreply.github.com>
2022-12-17 18:26:48 +0900

    Update page_c.py

--- pages/page_c.py
+++ pages/page_c.py
@@ -1,3 +1,26 @@
 import streamlit as st
 
+st.title("Page C")
 st.write("Page C")
+
+
+st.code('
+import platform
+print(f"Python Version: {platform.python_version()}")
+print(f'VER:{sys.version_info}')
+print('OS : '+ platform.system())
+print('OSリリース情報 : '+ platform.release())
+print('OSバージョン情報 : '+ platform.version())
+print('OSとバージョン : '+ platform.platform())
+
+
+
+import sys
+print('Pythonのバージョン情報 : '+ sys.version)
+import numpy as np
+print(np.__version__)#Numpyのバージョン確認
+import pandas as pd
+print(pd.__version__)#Pandasのバージョン確認
+
+
+')
