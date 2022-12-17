5fae53c4259d527fa5ca5778bce8da1d90c9c966
capris-tak <109158237+capris-tak@users.noreply.github.com>
2022-12-17 18:29:45 +0900

    Update page_c.py

--- pages/page_c.py
+++ pages/page_c.py
@@ -11,15 +11,15 @@ print('OS : '+ platform.system())
 print('OSリリース情報 : '+ platform.release())
 print('OSバージョン情報 : '+ platform.version())
 print('OSとバージョン : '+ platform.platform())
+''')
+st.write(f"Python Version: {platform.python_version()}")
 
 
-
+st.code('''
 import sys
 print('Pythonのバージョン情報 : '+ sys.version)
 import numpy as np
 print(np.__version__)#Numpyのバージョン確認
 import pandas as pd
 print(pd.__version__)#Pandasのバージョン確認
-
-
 ''')
