8500650007c88e34d6eb294378c834a2372fe386
capris-tak <109158237+capris-tak@users.noreply.github.com>
2022-12-18 10:56:42 +0900

    Add files via upload

---
+++ pages/型文字列.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import platform
+import sys
+
+st.title("01 システム")
+
+
+st.subheader("sys")
+st.code('''
+import sys
+print('Python Version : '+ sys.version)
+print(f'VER:{sys.version_info}')
+''')
+st.write('Python Version : '+ sys.version)
+st.write(f'VER : {sys.version_info}')
+
+
+
+st.subheader("platform")
+st.code('''import platform
+print(f"Python Version: {platform.python_version()}")
+print('OS : '+ platform.system())
+print('OSリリース情報 : '+ platform.release())
+print('OSバージョン情報 : '+ platform.version())
+print('OSとバージョン : '+ platform.platform())
+''')
+st.write(f"Python Version: {platform.python_version()}")
+st.write('OS : '+ platform.system())
+st.write('OSリリース情報 : '+ platform.release())
+st.write('OSバージョン情報 : '+ platform.version())
+st.write('OSとバージョン : '+ platform.platform())
+
+
