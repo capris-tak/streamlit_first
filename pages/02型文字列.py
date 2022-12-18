86e85aab955f3fa9ee0e0db9a047c25cf5854ad3
capris-tak <109158237+capris-tak@users.noreply.github.com>
2022-12-18 11:01:42 +0900

    Update and rename 型文字列.py to 02型文字列.py

---
+++ pages/02型文字列.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.title("02 型文字列")
+
+
+st.subheader(".isXXX")
+st.code('''
+print(r'That is \n \t \\ Carol\'s cat.')
+print('hello123'.isalpha())#英語のみ
+print('hello123'.isalnum())#英語と数字のみ
+print('123'.isdecimal())
+''')
+st.write(r'That is \n \t \\ Carol\'s cat.')
+st.write('hello123'.isalpha())
+st.write('hello123'.isalnum())
+st.write('123'.isdecimal())
+
+
+
+
