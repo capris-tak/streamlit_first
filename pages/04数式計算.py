e548641cb4614cb0630be29896fe5c1b9fcd51f2
capris-tak <109158237+capris-tak@users.noreply.github.com>
2022-12-18 13:07:50 +0900

    Update 04数式計算.py

--- pages/04数式計算.py
+++ pages/04数式計算.py
@@ -6,6 +6,12 @@ st.title("数式")
 
 st.write('Latex')
 
+st.write('')
+st.write(r'''
+    a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
+    \sum_{k=0}^{n-1} ar^k =
+    a \left(\frac{1-r^{n}}{1-r}\right)
+    ''')
 st.latex(r'''
     a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
     \sum_{k=0}^{n-1} ar^k =
