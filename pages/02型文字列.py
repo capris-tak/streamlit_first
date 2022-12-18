4b9351016820b6525286703717af2ef6c22e9824
capris-tak <109158237+capris-tak@users.noreply.github.com>
2022-12-18 11:03:25 +0900

    Update 02型文字列.py

--- pages/02型文字列.py
+++ pages/02型文字列.py
@@ -5,10 +5,10 @@ st.title("02 型文字列")
 
 st.subheader(".isXXX")
 st.code('''
-print(r'That is \n \t \\ Carol\'s cat.')
+#print(r'That is \n \t \\ Carol\'s cat.')
 print('hello123'.isalpha())#英語のみ
 print('hello123'.isalnum())#英語と数字のみ
-print('123'.isdecimal())
+print('123'.isdecimal())#数字のみ
 ''')
 st.write(r'That is \n \t \\ Carol\'s cat.')
 st.write('hello123'.isalpha())
