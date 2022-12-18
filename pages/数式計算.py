61469d59623e0c40e18c244b84c26668de14321c
capris-tak <109158237+capris-tak@users.noreply.github.com>
2022-12-18 12:55:09 +0900

    Update 数式計算.py

--- pages/数式計算.py
+++ pages/数式計算.py
@@ -2,8 +2,15 @@ import streamlit as st
 from sympy import *
 from sympy.abc import *
 
-st.write("Page B")
+st.title("数式")
 
+st.write('Latex')
+
+st.latex(r'''
+    a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
+    \sum_{k=0}^{n-1} ar^k =
+    a \left(\frac{1-r^{n}}{1-r}\right)
+    ''')
 
 #積分∫3𝑥2𝑑𝑥
-integrate(3*x**2, x)
+#integrate(3*x**2, x)
