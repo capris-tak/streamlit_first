e97bc6579483722b147153e5b499a556ef51719a
capris-tak <109158237+capris-tak@users.noreply.github.com>
2022-12-18 10:05:07 +0900

    Update page_c.py

--- pages/page_c.py
+++ pages/page_c.py
@@ -7,14 +7,12 @@ st.write("platform")
 
 st.code('''import platform
 print(f"Python Version: {platform.python_version()}")
-print(f'VER:{sys.version_info}')
 print('OS : '+ platform.system())
 print('OSリリース情報 : '+ platform.release())
 print('OSバージョン情報 : '+ platform.version())
 print('OSとバージョン : '+ platform.platform())
 ''')
 st.write(f"Python Version: {platform.python_version()}")
-st.write(f'VER:{sys.version_info}')
 st.write('OS : '+ platform.system())
 st.write('OSリリース情報 : '+ platform.release())
 st.write('OSバージョン情報 : '+ platform.version())
@@ -25,6 +23,7 @@ st.write("sys")
 st.code('''
 import sys
 print('Pythonのバージョン情報 : '+ sys.version)
+print(f'VER:{sys.version_info}')
 import numpy as np
 print(np.__version__)#Numpyのバージョン確認
 import pandas as pd
