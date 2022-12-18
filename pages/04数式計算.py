e426ff3a0f6e5ad995f2f056aa0b17a545dbc7c9
capris-tak <109158237+capris-tak@users.noreply.github.com>
2022-12-18 12:56:27 +0900

    Update and rename 数式計算.py to 04数式計算.py

--- pages/04数式計算.py
+++ pages/04数式計算.py
@@ -6,11 +6,13 @@ st.title("数式")
 
 st.write('Latex')
 
-st.latex(r'''
+st.code('''
+    st.latex(r'''
     a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
     \sum_{k=0}^{n-1} ar^k =
     a \left(\frac{1-r^{n}}{1-r}\right)
     ''')
+''')
 
 #積分∫3𝑥2𝑑𝑥
 #integrate(3*x**2, x)
